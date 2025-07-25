77b3298ad3e1efd46427facd669cf51995663642
bardahan96 <bardahan22@icloud.com>
2025-07-25 15:50:04 +0300

    just need to configure prev-arrow for finsih assignement 1

--- notes-app/src/Notes/NoteApp/NotesApp.jsx
+++ notes-app/src/Notes/NoteApp/NotesApp.jsx
@@ -89,13 +89,15 @@ console.log("this is the clicked name" , clickedName);
             <BrowserRouter>
 
                 <div className="noteHeader">
-                    <h1>My Note App</h1>
-                    <NavLink to=""><svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <h1 className="noteHeadLine">My Note App</h1>
+                    <NavLink to="">
+                        <button className="prevBtn"><svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M13.5 7H1M1 7L7 1M1 7L7 13" stroke="#4E61F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+</svg></button>
 </NavLink>
-                    
+                
                 </div>
+                <div className="underLine"></div>
 
                 <Routes>
                     <Route path="" element={ <SideBar isMobile={isMobile} noteName={handleClickedName} sharedStorage={updateForm}/>}/>
